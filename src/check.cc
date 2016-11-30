8ad599056527100536d0d4c21b3c302040f8d647
kentrino <kentrino@h-kento.jp>
2016-11-30 16:24:25 +0900

    (check) remove check4 function

--- src/check.cc
+++ src/check.cc
@@ -3,65 +3,6 @@
 #include <check.h>
 #include <miller_rabin.h>
 
-bool check4(int primes[], int n)
-{
-  int i, j;
-  int d[4][4];
-
-  for (i = 0; i < 4; i++) {
-    d[0][i] = primes[i];
-    for (j = 1; j < 4; j++) {
-      d[j][i] = d[j-1][i] * n;
-    }
-  }
-
-  int pairs[12][2] = {
-    {0, 1},
-    {1, 0},
-    {0, 2},
-    {2, 0},
-    {0, 3},
-    {3, 0},
-    {1, 2},
-    {2, 1},
-    {1, 3},
-    {3, 1},
-    {2, 3},
-    {3, 2},
-  };
-
-  int unused[6][2][2] = {
-    {{2, 3}, {3, 2}},
-    {{1, 3}, {3, 1}},
-    {{1, 2}, {2, 1}},
-    {{0, 3}, {3, 0}},
-    {{0, 2}, {2, 0}},
-    {{0, 1}, {1, 0}}
-  };
-
-  int number2, number3_1, number3_2, number4_1, number4_2;
-
-  for (i = 0; i < 12; i++) {
-    // 二桁の数のチェック
-    number2 = d[1][pairs[i][1]] + d[0][pairs[i][0]];
-    if (!MillerRabin::test(number2)) return false;
-
-    // 三桁の数のチェック
-    number3_1 = number2 + d[2][unused[i/2][0][0]];
-    number3_2 = number2 + d[2][unused[i/2][1][0]];
-    if (!MillerRabin::test(number3_1)) return false;
-    if (!MillerRabin::test(number3_2)) return false;
-
-    // 四桁の数のチェック
-    number4_1 = number3_1 + d[3][unused[i/2][0][1]];
-    number4_2 = number3_2 + d[3][unused[i/2][1][1]];
-    if (!MillerRabin::test(number4_1)) return false;
-    if (!MillerRabin::test(number4_2)) return false;
-  }
-
-  return true;
-}
-
 bool check3(const int primes[], int n)
 {
   const static int pairs[6][2] = {
