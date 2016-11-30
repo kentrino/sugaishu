51b381461dd3b7006741642f3f203648f5190bb4
kentrino <kentrino@h-kento.jp>
2016-11-30 16:24:01 +0900

    fix overflow bug

--- src/miller_rabin.cc
+++ src/miller_rabin.cc
@@ -8,13 +8,15 @@ namespace mp = boost::multiprecision;
 
 namespace MillerRabin {
   
+// 二乗してもint64_tを超えない値の最大値
+const int N_MAX = 3037000499;
 const int MEMO_MAX = 500000000;
 bool m_[MEMO_MAX] = {};
 bool memorized_[MEMO_MAX] = {};
 
 // TODO: これどうにかしたい
-inline int mod_power(int _p, int64_t y, int n);
-inline int64_t mod_power(int _p, int64_t y, int64_t n);
+inline int64_t mod_squre(int64_t x, int64_t n);
+inline int64_t mod_power(int p, int64_t d, int64_t n);
 
 bool test(int64_t n) {
   static int i, j;
@@ -47,25 +49,19 @@ bool test(int64_t n) {
     ++s;
   }
 
-  mp::int128_t x_int128;
-  int64_t x;
-  int64_t p;
+  static int64_t x;
+  static int p;
   
+  // TODO: これ減らす。。。？
   for (i = 0; i < max_tester_prime_index; ++i) {
     bool is_composite = true;
     p = Prime::list[i];
-    if (n < 2147483647) {
-      x = mod_power(p, d, static_cast<int>(n));
-    } else {
-      // use int128
-      x_int128 = mp::powm<mp::int128_t>(p, d, n);
-      x = static_cast<int64_t>(x_int128);
-    }
+    x = mod_power(p, d, n);
     is_composite = is_composite && (x % n != 1);
 
     for (j = 0; j < s; j++) {
       is_composite = is_composite && (x % n != n - 1);
-      x = x * x % n;
+      x = mod_squre(x, n);
     }
 
     if (is_composite) {
@@ -90,22 +86,50 @@ void init() {
   std::uninitialized_fill_n(memorized_, MEMO_MAX, false);
 }*/
 
-inline int mod_power(int _p, int64_t y, int n) {
+inline int64_t mod_power_small(int64_t p, int64_t d, int64_t n) {
   static int64_t res;
-  static int64_t p;
 
   res = 1;
-  p = _p;
 
-  while (y >= 1) {
-    if (y & 1) {
+  while (d >= 1) {
+    if (d & 1) {
       res = (res * p) % n;
     }
-    y >>= 1;
+    d >>= 1;
     p = (p * p) % n;
   }
 
-  return static_cast<int>(res);
+  return res;
+}
+
+inline int64_t mod_power(int p, int64_t d, int64_t n) {
+  static int64_t x;
+  static mp::int128_t x_int128;
+
+  static int64_t p_int64;
+  p_int64 = p;
+
+  if (n < N_MAX) {
+    x = mod_power_small(p_int64, d, n);
+  } else {
+    x_int128 = mp::powm<mp::int128_t>(p, d, n);
+    x = static_cast<int64_t>(x_int128);
+  }
+  return x;
+}
+
+inline int64_t mod_squre(int64_t x, int64_t n) {
+  static mp::int128_t x_int128;
+
+  if (n < N_MAX) {
+    x = x * x % n;
+  } else {
+    x_int128 = x;
+    x_int128 = x_int128 * x_int128 % n;
+    x = static_cast<int64_t>(x_int128);
+  }
+
+  return x;
 }
 
 }
